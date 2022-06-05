b760888fe78b3e88a50d8b35a3aa13e1881a64de
Shahzaib-A530 <bsef19a530@pucit.edu.pk>
2022-06-05 23:15:34 +0500

    third Commit

--- Customizedlistview/app/src/main/java/com/first/customizedlistview/MainActivity.java
+++ Customizedlistview/app/src/main/java/com/first/customizedlistview/MainActivity.java
@@ -3,9 +3,12 @@ package com.first.customizedlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView    t1,t2,t3;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
